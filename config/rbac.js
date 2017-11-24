60f3c4cf30f55aa6f634c8e01bba923b48d2a781
dainli <dainli@outlook.com>
2017-11-24 14:31:27 +0800

    redefine initailize permissions and roles

--- config/rbac.js
+++ config/rbac.js
@@ -2,14 +2,17 @@
 
 exports.permissions = [
   // action_resource
-  { name: 'query_article', alias: '访问文章管理' },
-  { name: 'query_tag', alias: '访问标签管理' },
-  { name: 'query_collect', alias: '访问收藏管理' },
-  { name: 'query_user', alias: '访问用户管理' },
-  { name: 'query_setting', alias: '访问通用设置' },
+  { name: '/admin', alias: '访问文章管理' },
+  { name: '/admin/tag', alias: '访问标签管理' },
+  { name: '/admin/collect', alias: '访问收藏管理' },
+  { name: '/admin/user', alias: '访问用户管理' },
+  { name: '/admin/setting', alias: '访问通用设置' },
+  { name: '/admin/user/new', alias: '创建用户' },
+  { name: '/admin/setting/newrole', alias: '创建角色' },
+  { name: '/admin/setting/modify', alias: '修改角色权限' },
 ];
 
 exports.roles = [
   { name: 'admin', alias: '管理员', grants: exports.permissions.map(item => item.name) },
-  { name: 'user', alias: '普通用户', grants: [ 'query_article', 'query_tag', 'query_collect' ] },
+  { name: 'user', alias: '普通用户', grants: [ '/admin', '/admin/tag', '/admin/collect' ] },
 ];
